14adf3e21aabb6e4aa91b4f942c94fef6e9715d9
Mehmet Basbayandur <mhmtbsbyndr@hotmail.com>
2026-05-07 02:46:59 +0200

    Update v4 module exports

--- src/v4/mod.rs
+++ src/v4/mod.rs
@@ -1,13 +1,15 @@
 pub mod budget;
 pub mod cache;
 pub mod context;
+pub mod context_items;
 pub mod contracts;
 pub mod errors;
 pub mod tools;
 pub mod trace;
 
 pub use budget::{BudgetCheck, BudgetRemaining, BudgetState};
-pub use context::{ContextAssembler, ContextItem, ContextItemKind, ContextPackage, ExcludedContext};
+pub use context::{ContextAssembler, ContextPackage};
+pub use context_items::{ContextItem, ContextItemKind, ExcludedContext};
 pub use contracts::{StopCondition, TaskContract, TaskIntent, TaskScope};
 pub use errors::{V4Error, V4Result};
 pub use tools::{CheckBudgetRequest, CreateTaskContractRequest, GetTaskContextRequest, V4Tools};
